98b7557b543aa5ce2fff3443823dd0730dde2592
Francisco Javier Huete <fjhuete.m@gmail.com>
2025-03-04 12:37:30 +0000

    Modificaciones para usar la aplicación con una base de datos MariaDB

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ RUN chmod +x /usr/local/bin/docker-entrypoint.sh \
 && pip install --no-cache-dir --break-system-packages -r requirements.txt
 EXPOSE 3000
 ENV URL_BASE=http://localhost
+ENV DB_HOST=localhost
+ENV DB_USER=usuario
+ENV DB_PASS=asdasd
+ENV DB=django
 ENV DJANGO_MAIL=mail@mail.org
 ENV DJANGO_USER=admin
 ENV DJANGO_PASS=admin
--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASS"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': '',
     }
 }
 
